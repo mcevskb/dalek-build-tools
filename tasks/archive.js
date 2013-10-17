340804b9275ceaf53f190e31270837cc113eddf6
asciidisco <public@asciidisco.com>
2013-10-17 12:47:14 +0200

    feat(task) Added archive task

---
+++ tasks/archive.js
@@ -0,0 +1,29 @@
+/* jshint camelcase: false */
+module.exports = function(grunt) {
+  'use strict';
+
+  // archives the docs if a new version appears
+  grunt.registerTask('archive', function () {
+    var done = this.async();
+    var options = this.options();
+    var file = options.file;
+
+    grunt.util.spawn({cmd: 'git', args: ['describe', '--abbrev=0', '--tags']}, function (error, result) {
+      var lastTag = result.toString();
+      if (grunt.file.isFile('_raw/docs/' + lastTag + '/' + file)) {
+        grunt.log.ok('Nothing to archive');
+        done();
+        return true;
+      }
+
+      if (!grunt.file.isDir('_raw/docs/' + lastTag)) {
+        grunt.file.mkdir('_raw/docs/' + lastTag);
+      }
+
+      grunt.file.copy('report/docs/' + file, '_raw/docs/' + lastTag + '/' + file);
+      grunt.log.ok('Archived document with version: ' + lastTag);
+      done();
+    });
+  });
+
+};
