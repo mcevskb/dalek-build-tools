096aa25df20379d97a7a722357f6b96fbaad7f31
asciidisco <public@asciidisco.com>
2013-10-22 08:45:41 +0200

    Fixed archive task in case of multi files

--- tasks/archive.js
+++ tasks/archive.js
@@ -6,22 +6,25 @@ module.exports = function(grunt) {
   grunt.registerTask('archive', function () {
     var done = this.async();
     var options = this.options();
-    var file = options.file;
+    var files = options.files || [options.file];
 
     grunt.util.spawn({cmd: 'git', args: ['describe', '--abbrev=0', '--tags']}, function (error, result) {
       var lastTag = result.toString();
-      if (grunt.file.isFile('_raw/docs/' + lastTag + '/' + file)) {
-        grunt.log.ok('Nothing to archive');
-        done();
-        return true;
-      }
+      files.forEach(function (file) {
+        if (grunt.file.isFile('_raw/docs/' + lastTag + '/' + file)) {
+          grunt.log.ok('Nothing to archive');
+          done();
+          return true;
+        }
 
-      if (!grunt.file.isDir('_raw/docs/' + lastTag)) {
-        grunt.file.mkdir('_raw/docs/' + lastTag);
-      }
+        if (!grunt.file.isDir('_raw/docs/' + lastTag)) {
+          grunt.file.mkdir('_raw/docs/' + lastTag);
+        }
 
-      grunt.file.copy('report/docs/' + file, '_raw/docs/' + lastTag + '/' + file);
-      grunt.log.ok('Archived document with version: ' + lastTag);
+        grunt.file.copy('report/docs/' + file, '_raw/docs/' + lastTag + '/' + file);
+        grunt.log.ok('Archived document with version: ' + lastTag);
+      });
+
       done();
     });
   });
